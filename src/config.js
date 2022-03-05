64f31855f3acf64141899c1b6d80b11fd02cf953
josejuniorpgx <yunipgx@protonmail.com>
2022-03-04 23:50:14 -0600

    Agregue estilos1

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ config()
 //Esto hace que cuando llame al archivo estp lea en .env
 export const MONGODB_URI= "mongodb://localhost:27017/crud-mongo" ||'mongodb://localhost/test';
 //console.log(process.env.MONGODB_URI);
+// se cambia el mongodb
 //proces, sirve para acceder a variable en el proceso principal. Que es mi computador
 
 export const PORT = process.env.PORT ||4000;
