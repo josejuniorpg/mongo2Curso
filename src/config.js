613364438b95e5e9a63897ed1112ba6b70b4d6bb
josejuniorpgx <yunipgx@protonmail.com>
2022-03-07 00:51:36 -0600

    Cambie en env. y el babel y agregue el start

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ import {config} from "dotenv";
 config()
 
 //Esto hace que cuando llame al archivo estp lea en .env
-export const MONGODB_URI= "mongodb://localhost:27017/crud-mongo" ||'mongodb://localhost/test';
+export const MONGODB_URI=  process.env.MONGODB_URI || "mongodb://localhost/test";
+//export const MONGODB_URI= "mongodb://localhost:27017/crud-mongo" ||'mongodb://localhost/test';
 //console.log(process.env.MONGODB_URI);
 // se cambia el mongodb
 //proces, sirve para acceder a variable en el proceso principal. Que es mi computador
